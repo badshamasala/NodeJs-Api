732c7b1b74616fec3aebda5200af2c2e58f1a21a
Aarashid1 <rashid.khan@resettech.in>
2023-05-10 16:56:41 +0530

    ss

--- models/user_model.js
+++ models/user_model.js
@@ -12,21 +12,21 @@ const userSchema = new Schema({
   updated_at: { type: Date, default: null },
 },{versionKey:false});
 
-userSchema.pre('save', async function (next) {
-  try {
-    // only hash the password if it has been modified (or is new)
-    if (!this.isModified('password')) {
-      return next();
-    }
-    // hash the password using the salt
-    const hash = await bcrypt.hash(this.password, 10);
-    // override the cleartext password with the hashed one
-    this.password = hash;
-    next();
-  } catch (error) {
-    return next(error);
-  }
-});
+// userSchema.pre('save', async function (next) {
+//   try {
+//     // only hash the password if it has been modified (or is new)
+//     if (!this.isModified('password')) {
+//       return next();
+//     }
+//     // hash the password using the salt
+//     const hash = await bcrypt.hash(this.password, 10);
+//     // override the cleartext password with the hashed one
+//     this.password = hash;
+//     next();
+//   } catch (error) {
+//     return next(error);
+//   }
+// });
 
 const User = model('User', userSchema);
 
